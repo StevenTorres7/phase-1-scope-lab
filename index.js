233f8f9aab44047f804c8643a5d8bac7d070e716
Steven Torres <StevenTorres7@yahoo.com>
2022-10-20 20:54:28 -0400

    commit tree c4f9859d64d25213d454499fdc4c883e68c33d04

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+var setBestCustomer;
+const leastFavoriteCustomer = "Steven";
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "HakunaMatata";
+}
